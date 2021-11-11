0eca2701f1dadddc5a678de06dd69f11daac634d
RehamMobarak <reham.a.mobarak@gmail.com>
2021-11-12 00:07:34 +0200

    add show category posts

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -46,7 +46,9 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('posts.index', [
+            "posts" => $category->posts
+        ]);
     }
 
     /**
