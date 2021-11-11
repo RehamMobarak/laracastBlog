852a136d1e6c9935a3a7957e0d70bebe231634d2
RehamMobarak <reham.a.mobarak@gmail.com>
2021-11-11 19:05:11 +0200

    add run factories to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // if you don't REFRESH DB you'll need
+            // User::truncate();
+            // Post::truncate();
+            // Category::truncate();
+
+        Post::factory()->create();
+
+      //  $user = User::factory()->create();
+
+        // $family = Category::create([
+        //     "name" => "Family",
+        //     "slug" => "family",
+        // ]);
+
+
+        // $personal = Category::create([
+        //     "name" => "Personal",
+        //     "slug" => "personal",
+        // ]);
+
+
+        // $work = Category::create([
+        //     "name" => "Work",
+        //     "slug" => "work",
+        // ]);
+
+        // Post::create([
+        //     "user_id" => $user->id,
+        //     "category_id" => $family->id,
+        //     "slug" => "my-family-post",
+        //     "title" => "My family post",
+        //     "excerpt" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit",
+        //     "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quia eius deserunt autem, esse dolorem est nisi fugit earum explicabo temporibus tenetur perferendis quibusdam in hic repudiandae corporis odit. Placeat. Placeat mollitia atque ipsum dolorem provident veniam sint enim? Architecto maiores quam aperiam modi tenetur molestias animi illum necessitatibus optio, voluptate asperiorest."
+        // ]);
+
+        // Post::create([
+        //     "user_id" => $user->id,
+        //     "category_id" => $work->id,
+        //     "slug" => "my-work-post",
+        //     "title" => "My work post",
+        //     "excerpt" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit",
+        //     "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quia eius deserunt autem, esse dolorem est nisi fugit earum explicabo temporibus tenetur perferendis quibusdam in hic repudiandae corporis odit. Placeat. Placeat mollitia atque ipsum dolorem provident veniam sint enim? Architecto maiores quam aperiam modi tenetur molestias animi illum necessitatibus optio, voluptate asperiorest."
+        // ]);
+
+        // Post::create([
+        //     "user_id" => $user->id,
+        //     "category_id" => $personal->id,
+        //     "slug" => "my-personal-post",
+        //     "title" => "My personal post",
+        //     "excerpt" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit",
+        //     "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quia eius deserunt autem, esse dolorem est nisi fugit earum explicabo temporibus tenetur perferendis quibusdam in hic repudiandae corporis odit. Placeat. Placeat mollitia atque ipsum dolorem provident veniam sint enim? Architecto maiores quam aperiam modi tenetur molestias animi illum necessitatibus optio, voluptate asperiorest."
+        // ]);
     }
 }
