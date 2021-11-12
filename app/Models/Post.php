bf9c10510a6d733c9761c56f11e8414b97154873
RehamMobarak <reham.a.mobarak@gmail.com>
2021-11-12 02:52:33 +0200

    add $with to post model for eagerloading

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,9 @@ class Post extends Model
 {
     use HasFactory;
 
+    // eagerLoading with every post query as a default
+    protected $with = ['category', 'author'];
+
     public function category()
     {
         return  $this->belongsTo(Category::class);
