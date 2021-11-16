508e238c8ed65803486018c979d5d64d1500fd6b
RehamMobarak <reham.a.mobarak@gmail.com>
2021-11-16 06:20:29 +0200

    add: post-grid comp and changes to use it

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -6,8 +6,8 @@ within that component. --}}
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <!-- flex (is added to equally distribute the 2 main divs (img | post body)) -->
+    <!-- image -->
     <div class="py-6 px-5 lg:flex">
-        <!-- image -->
         <div class="flex-1 lg:mr-8">
             <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
@@ -15,26 +15,17 @@ within that component. --}}
         <!-- text -->
         <!-- flex  flex-col (is added to align the footer to end on the same line with img on the left) -->
         <div class="flex-1 flex flex-col justify-between">
-            <!-- tags buttons above the title -->
-
             <header class="mt-8 lg:mt-0">
-                <!-- space-x-2 (set a space between items -btns- on the line) -->
-
                 <div class="space-x-2">
-                    <a href="/categories/{{ $post->category->slug }} "
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
+                    <x-category-button :category="$post->category" />
                 </div>
-                <!-- head title & date -->
 
                 <div class="mt-4">
-                    <!-- head title -->
-
                     <h1 class="text-3xl">
-                        {{ $post->title }}
+                        <a href="/posts/{{ $post->slug }}">
+                            {{ $post->title }}
+                        </a>
                     </h1>
-
-                    <!-- date -->
 
                     <span class="mt-2 block text-gray-400 text-xs">
                         Published <time>{{ $post->created_at->diffForHumans() }}</time>
@@ -42,29 +33,19 @@ within that component. --}}
                 </div>
             </header>
 
-            <!-- content -->
-
-            <div class="text-sm mt-2">
-                <p>
-                    {{ $post->excerpt }}
-                </p>
+            <div class="text-sm mt-2 space-y-4">
+                {!! $post->excerpt !!}
             </div>
-
-            <!-- lary img and read more -->
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">
-                            <small> <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
-                            </small>
+                            <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
                         </h5>
-                        <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
-
-                <!-- read more -->
 
                 <div class="hidden lg:block">
                     <a href="/posts/{{ $post->slug }}"
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,40 +1,10 @@
 <x-layout>
-    {{-- @foreach ($posts as $post )
-    <h1>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-    </h1>
-    <h4>
-        By "<a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>" in <a
-            href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-    </h4>
-    <p>
-        {{ $post->excerpt }}
-    </p>
-    <hr>
-    @endforeach --}}
-
     @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <!-- first post -->
-        <x-post-featured-card :post="$posts[0]"/>
-        <!-- -------------------------------------------------------------- -->
-        <!-- second row of posts (2 posts)-->
-
-        <div class="lg:grid lg:grid-cols-2">
-            <!-- row of two cols/posts -->
-            <x-post-cards-middle />
-            <x-post-cards-middle />
-        </div>
-
-        <!-- -------------------------------------------------------------- -->
-        <!-- third row of posts (3 posts)-->
-
-        <div class="lg:grid lg:grid-cols-3">
-            <!-- row of 3 cols/posts -->
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+        @if ($posts->count())
+        <x-posts-grid :posts="$posts"/>
+        @else
+        <p class="text-center"> No posts yet. Please check later.</p>
+        @endif
     </main>
-
 </x-layout>
