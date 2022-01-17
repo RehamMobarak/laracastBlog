c28286ba12fe1d1f0dc501f0b1472248f1a783df
RehamMobarak <reham.a.mobarak@gmail.com>
2022-01-17 05:51:27 +0200

    add: paginate homepage results

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
         //! remember to pass all filters from the request
 
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search','category','author']))->get(),
+            'posts' => Post::latest()->filter(request(['search','category','author']))->paginate(6),
         ]);
     }
 
